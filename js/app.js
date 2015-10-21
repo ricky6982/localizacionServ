de968fbd214816e5ae435061962dba1b5a9ca955
RickySSD <ricky6982@gmail.com>
2015-10-21 01:03:47 -0300

    Se creo directiva para mostrar los lugares para ser añadidos en el arco seleccionado.
    Se plantea refactorizar el codigo para separar el AppCtrl en
    dos Controladores:
     - AppCtrl: Contendra los datos que necesitan ser compartidos entre los
       dos controladores que se describen a continuación.
     - ServicioCtrl: Controlador para ABM del listado de Servicios
     - LocalizacionCtrl: Controlador encargado de asignar los servicios en
       el arco ("edge") seleccionado.
    
    
    Signed-off-by: RickySSD <ricky6982@gmail.com>

--- js/app.js
+++ js/app.js
@@ -1,21 +1,53 @@
 var app = angular.module('app', []);
 
+// ========================
+// Definición de Directivas
+// ========================
+app.directive('tsPanelServicio', ['$filter', function ($filter) {
+    return {
+        restrict: 'AE',
+        replace: true,
+        templateUrl: "panel-servicio.html",
+        scope: {
+            listadoServicios: '=listadoServicios',
+            lugar: '=lugar'
+        },
+        controller: ['$scope',
+            function($scope){
+                $scope.updateSublistaServicios = function(){
+                    $scope.sublistaServicios = $filter('filter')($scope.listadoServicios, {id: $scope.lugar.idCategoria })[0].listado;
+                };
+
+                // $scope.updateSublistaServicios();
+            }
+        ]
+    };
+}]);
+
+
+// ===========================
+// Definición de Controladores
+// ===========================
 app.controller('AppCtrl', [
     '$scope',
     function($scope){
+
+        // =================================
+        // ABM de Servicios y sus Categorias
+        // =================================
         $scope.servicio = {
             selected: -1,
             nuevo: function(){
                 servicio = {};
                 servicio.nombre = $scope.servicioModal.nombre;
-                servicio.id = generateId($scope.servicios);
+                servicio.id = generateId($scope.listadoServicios);
                 servicio.items = [];
-                $scope.servicios.push(servicio);
+                $scope.listadoServicios.push(servicio);
                 $scope.servicioModal.nombre = "";
             },
             borrar: function(index){
                 if (confirm('¿Esta seguro de que desea eliminar el servicio y todos sus items?')) {
-                    $scope.servicios.splice(index, 1);
+                    $scope.listadoServicios.splice(index, 1);
                 }
             },
             editar: function(index){
@@ -33,7 +65,7 @@ app.controller('AppCtrl', [
                 $scope.servicioSelected = idServ;
             },
             guardar: function(){
-                var servicio = _.find($scope.servicios, {id: $scope.servicioSelected});
+                var servicio = _.find($scope.listadoServicios, {id: $scope.servicioSelected});
                 var item = angular.copy($scope.itemModal);
                 item.id = generateId(servicio.items);
                 servicio.items.push(item);
@@ -66,7 +98,7 @@ app.controller('AppCtrl', [
 
             borrar: function(idServ, indexItem){
                 if (confirm('¿Esta seguro que desea eliminar el Item?')) {
-                    var servicio = _.find($scope.servicios, {id: idServ});
+                    var servicio = _.find($scope.listadoServicios, {id: idServ});
                     servicio.items.splice(indexItem, 1);
                 }
             }
@@ -81,23 +113,56 @@ app.controller('AppCtrl', [
             }
         }
 
-        $scope.servicios = [
+        // ==============================================
+        // Estructura de Datos de los servicios ordenados 
+        // por sus categorias
+        // ==============================================
+        $scope.listadoServicios = [
             {
                 id: 1,
                 nombre: 'Boleterias',
                 items: [
                     {
                         id: 10,
-                        nombre: 'Boleteria El Quiaqueño',
+                        nombre: 'El Quiaqueño',
                         descripcion: 'Viajes al interior de la Provincia.'
                     },
                     {
                         id: 11,
-                        nombre: 'Boleteria Balut',
+                        nombre: 'Balut',
                         descripcion: 'Viajes a todo el Pais.'
                     }
                 ]
             }
         ];
+
+        // ==============================================
+        // Logica para la interfaz visual para posicionar
+        // un servicio sobre un arco
+        // ==============================================
+        $scope.itemAux = {
+            idCategoria: null,
+            idServicio: null,
+            categoria: "",
+            servicio: "",
+            distancia: ""
+        };
+
+        $scope.serviciosItem = function(){
+            return [{id: 1, nombre: "test 1"},{id: 2, nombre: "test 2"},{id: 3, nombre: "test 3"}];
+        };
+
+        $scope.guiLocalizar = {
+            izq: [{idCategoria: null, idServicio: null, categoria: "", servicio:""}],
+            der: [],
+            add: function(direccion){
+                console.log('Agregregando item a la ' + direccion);
+            },
+            remove: function(item){
+                console.log('Removiendo item ' + item);
+            }
+        };
+
+
     }
 ]);
